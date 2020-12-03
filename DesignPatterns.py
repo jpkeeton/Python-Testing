33f4645379498f16cdf1588598062adc45205280
JeremyPK <jpkeeton@gmail.com>
2020-12-03 08:40:48 -0800

    Design Patterns Notes
    this looks interesting!

---
+++ DesignPatterns.py
@@ -0,0 +1,10 @@
+print('these are the SOLID Principles, a few of the design patterns that can be used in test automation')
+
+
+# What are the principles of automation?
+# SOLID Principles
+# SRP – Single Responsibility Principle.
+# OCP – Open/Closed Principle.
+# LSP – Liskov Substitution Principle.
+# ISP – Interface Segregation Principle.
+# DIP – Dependency Inversion Principle.
